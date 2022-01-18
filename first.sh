c226d9f761a7966810feb054f2c9678986230caa
Harsh <harshdeep292001@gmail.com>
2022-01-18 22:29:32 +0530

    updaed config and first.sh

--- first.sh
+++ first.sh
@@ -1,3 +1,3 @@
 pacman -S yay fakeroot
 
-yay -S telegram-desktop visual-studio-code-bin python python3-pip gnu-gcc brave-beta-bin spotify-adblock-git whatsdesk-bin teams pulseaudio-ladspa pulseaudio-equalizer-ladspa
+yay -S telegram-desktop visual-studio-code-bin python python3-pip gnu-gcc brave-beta-bin spotify-adblock-git whatsdesk-bin teams pulseaudio-ladspa pulseaudio-equalizer-ladspa git
